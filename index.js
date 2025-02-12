a0d2ca3030ba0a1c7dcba8f94939a8846b17f254
joaovnpt <joaoviorguiotti8@gmail.com>
2025-02-12 09:45:20 -0300

    Implement average gains and losses function

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const axios = require("axios");
 
 const SYMBOL = "BTCUSDT";
-const BUY_PRICE = 95900;
-const SELL_PRICE = 96600;
+const PERIOD = 14;
 
 const TEST_API_URL = "http://testnet.binance.vision";
+
+function averages(prices, period, startIndex) {
+  let gains = 0,
+    losses = 0;
+
+  for (let i = 0; i < period && i + startIndex < prices.length; i++) {
+    const diff = prices[i + startIndex] - prices[i + startIndex - 1];
+    if (diff >= 0) 
+      gains += diff;
+    else 
+      losses += Math.abs(diff);
+  }
+
+  let avgGains = gains / period
+  let avgLosses = losses / period
+  return { avgGains, avgLosses }
+}
 
 let isOpenned = false; //Estar aberto, na linguagem trader é ter comprado uma ação
 
 async function start() {
   const { data } = await axios.get(
-    TEST_API_URL + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL
+    TEST_API_URL + "/api/v3/klines?limit=100&interval=15m&symbol=" + SYMBOL
   );
   const candle = data[data.length - 1];
   const price = parseFloat(candle[4]);
@@ -18,16 +34,15 @@ async function start() {
   console.log("BTC Price : " + price);
   setTimeout(() => console.log("Refreshing..."), 500);
 
-
   if (price <= BUY_PRICE && isOpenned === false) {
     isOpenned = true;
     console.log("buying...");
   } else if (price >= SELL_PRICE && isOpenned === true) {
-    isOpenned = false
+    isOpenned = false;
     console.log("selling...");
   } else {
-    console.log('waiting')
+    console.log("waiting");
   }
 }
 
-setInterval(start, 1000);
+setInterval(start, 3000);
